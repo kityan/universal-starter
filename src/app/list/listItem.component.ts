b778a35483af110d09318747d6043646162ea706
Pavel Kityan <pavel@kityan.ru>
2018-04-10 15:02:20 +0300

    loader icon

--- src/app/list/listItem.component.ts
+++ src/app/list/listItem.component.ts
@@ -4,12 +4,12 @@ import { NgStyle, NgClass } from '@angular/common';
 @Component({
   selector: 'app-list-item',
   template: `
-    <p [ngStyle]="{'background': this.item.color}" [ngClass]="{'processing': this.processing}">
+    <p [ngStyle]="{'background-color': this.item.color}" [ngClass]="{'loader': this.processing}">
       {{this.item.title}}
       <button [disabled]="this.processing" (click)="onDeleteButtonClick()">delete</button>
     </p>
   `,
-  styles: ['p {padding: 10px;}', 'p.processing {opacity: 0.2}']
+  styles: ['p {padding: 10px; border: 1px solid #000;}', 'p.loader {background-color: #FFF !important}']
 })
 export class ListItemComponent implements OnInit {
   @Input() public processing: boolean;
